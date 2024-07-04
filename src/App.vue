72108b6e73ad3c1c111113c1b158cbf1c0696d8d
johnlin <john830110@gmail.com>
2024-07-04 21:07:24 +0800

    fix:

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+import { RouterView } from "vue-router";
+import { NavBar } from './components/NavBar.vue'
 </script>
 
 <template>
   <v-app>
+    <NavBar />
+    111
     <v-main>
-      <router-view />
+      <RouterView />
     </v-main>
   </v-app>
 </template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app-bar>
+    <v-toolbar-title>GenAI Star Demo</v-toolbar-title>
+    <v-spacer />
+
+  </v-app-bar>
+  <v-btn>登入</v-btn>
+</template>
+
+<script setup lang="ts">
+
+</script>
